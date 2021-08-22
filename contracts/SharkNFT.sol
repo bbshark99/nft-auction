516704bc87d55751f7f6cb374718b02768fe8a28
bbshark99 <blue.shark99@outlook.com>
2021-08-22 08:05:01 -0500

    Added checking nft allowance before transferring

--- contracts/SharkNFT.sol
+++ contracts/SharkNFT.sol
@@ -24,6 +24,11 @@ contract SharkNFT is ERC721URIStorage, Ownable {
         governance[msg.sender] = true;
     }
 
+    modifier onlyLiveToken(uint256 _nftId) {
+        require(ownerOf(_nftId) != address(0), "Invalid NFT");
+        _;
+    }
+
     function addMinter(address _minter) public onlyOwner {
         require(_minter != address(0), "Invalid address");
 
@@ -55,15 +60,21 @@ contract SharkNFT is ERC721URIStorage, Ownable {
         return nft.id;
     }
 
-    function burn(uint256 _nftId) external {
+    function burn(uint256 _nftId) external onlyLiveToken(_nftId) {
         require(_exists(_nftId), "Non existed NFT");
 
         _burn(_nftId);
+
+        sharks[_nftId].id = 0;
+        sharks[_nftId].name = '';
+        sharks[_nftId].uri = '';
+
+        totalSharks--;
     }
 
-    function transfer(uint256 _nftId, address _target) external {
+    function transfer(uint256 _nftId, address _target) external onlyLiveToken(_nftId) {
         require(_exists(_nftId), "Non existed NFT");
-        require(ownerOf(_nftId) == msg.sender || getApproved(_nftId) == msg.sender, "Not NFT owner");
+        require(ownerOf(_nftId) == msg.sender || getApproved(_nftId) == msg.sender, "Not approved");
         require(_target != address(0), "Invalid address");
 
         _transfer(ownerOf(_nftId), _target, _nftId);
